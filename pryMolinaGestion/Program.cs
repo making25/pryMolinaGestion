28f5a6eff644dd271775272fa2effa6c10d389c4
Making25 <martinpvscba@gmail.com>
2025-09-12 20:27:47 -0300

    sql

--- pryMolinaGestion/Program.cs
+++ pryMolinaGestion/Program.cs
@@ -11,7 +11,11 @@ namespace pryMolinaGestion
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
             Application.Run(new gestionInventario());
+
+            //Application.Run(new GestionDeProductos());
+
         }
     }
 }
